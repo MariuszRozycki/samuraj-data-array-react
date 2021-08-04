4f15e217d08fb6e21bffce01ab6e44abbb0c6993
MariuszRozycki <mariansc83@icloud.com>
2021-08-04 14:50:23 +0200

    Add data-array-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//Pseudo rosnaca baza danych
+const data = [
+  {
+    id: 1,
+    title: 'Massage nr 1',
+    body: 'Content av massasge nr 1 ...'
+  },
+  {
+    id: 2,
+    title: 'Massage nr 2',
+    body: 'Content av massasge nr 2 ...'
+  }
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Message nr ${index}`,
+    body: `Content av massage nr ${index}`
+  })
+  // console.log(data);
+}, 5000)
+
 class App extends Component {
+  state = {
+    comments: [...data]
+  }
+
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      this.setState({
+        comments: [...data]
+      })
+      console.log('Wywolana sie metoda getData()');
+    } else {
+      console.log('Nie aktalizuje');
+    }
+  }
+
+  componentDidMount() {
+    this.idInterval = setInterval(this.getData, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idInterval);
+  }
+
   render() {
+
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {comments.reverse()}
       </div>
     );
   }
